c7a5c85ed6a9e975528c6f491f0c3711bac5bd2c
sarah012-210 <148900173+sarah012-210@users.noreply.github.com>
2025-06-18 20:55:49 +0200

    Delete flu test directory

--- flu test/flu_model/dss044.R
+++
@@ -1,8 +0,0 @@
-# dss044.R
-dss044 <- function(xl, xu, nx, f, fx, nl, nu) {
-  fxx <- rep(0, nx)
-  dx <- (xu - xl)/(nx - 1)
-  fxx[2:(nx-1)] <- (f[3:nx] - 2*f[2:(nx-1)] + f[1:(nx-2)]) / (dx^2)
-  return(fxx)
-}
-
